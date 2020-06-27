d23f92fe33be83b71afdfb51bf43f700edab2d1d
Saad Ismail <saad.ismail@careem.com>
2020-06-27 16:21:03 +0500

    Added test for Five for Amount

--- src/test/java/dojo/supermarket/model/SupermarketTest.java
+++ src/test/java/dojo/supermarket/model/SupermarketTest.java
@@ -136,4 +136,39 @@ public class SupermarketTest {
 
         Assert.assertEquals(Arrays.asList(expectedItem1), receipt.getItems());
     }
+
+    @Test
+    public void testFiveForAmount() {
+        // SETUP
+        SupermarketCatalog catalog = new FakeCatalog();
+        Product toothbrush = new Product("toothbrush", ProductUnit.Each);
+        catalog.addProduct(toothbrush, 0.99);
+        Product apples = new Product("apples", ProductUnit.Kilo);
+        catalog.addProduct(apples, 1.99);
+        Product tomatoes = new Product("cherry tomatoes", ProductUnit.Each);
+        catalog.addProduct(tomatoes, 0.69);
+        Product rice = new Product("rice", ProductUnit.Each);
+        catalog.addProduct(rice, 2.49);
+
+        Product toothpaste = new Product("toothpaste", ProductUnit.Each);
+        catalog.addProduct(toothpaste, 1.79);
+
+        Teller teller = new Teller(catalog);
+        teller.addSpecialOffer(new Offer(SpecialOfferType.FiveForAmount, toothpaste, 7.49));
+
+        ShoppingCart cart = new ShoppingCart();
+        cart.addItemQuantity(toothpaste, 5);
+
+        // ACT
+        Receipt receipt = teller.checksOutArticlesFrom(cart);
+
+        // ASSERT
+        assertEquals(7.49, receipt.getTotalPrice(), 0.01);
+        assertEquals(Arrays.asList(new Discount(toothpaste, "5 for 7.49", (7.49 - catalog.getUnitPrice(toothpaste) * 5.0))), receipt.getDiscounts());
+        assertEquals(1, receipt.getItems().size());
+
+        ReceiptItem expectedItem1 = new ReceiptItem(toothpaste, 5, catalog.getUnitPrice(toothpaste));
+
+        Assert.assertEquals(Arrays.asList(expectedItem1), receipt.getItems());
+    }
 }
