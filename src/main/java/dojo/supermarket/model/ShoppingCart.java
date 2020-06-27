2a803900d05e6751108c8fc0a4486de335945b0a
Sauban bin Usman <saubanbinusman@yahoo.com>
2020-06-28 03:33:42 +0500

    Migrated to LinkedHashMap from HashMap to preserve order of insertion of cart items

--- src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -3,12 +3,13 @@ package dojo.supermarket.model;
 import dojo.supermarket.offer.BundledOffer;
 import dojo.supermarket.offer.Offer;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 public class ShoppingCart {
 
-    Map<Product, Double> productQuantities = new HashMap<>();
+    Map<Product, Double> productQuantities = new LinkedHashMap<>();
     Map<Product, Boolean> isOfferApplied = new HashMap<>();
 
     void addItem(Product product) {
