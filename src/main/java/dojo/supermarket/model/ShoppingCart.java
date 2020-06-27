3b74dd5ee712350530de0108b4196371342ad0e7
lamak <lamak.qaizar@careem.com>
2020-06-27 20:52:31 +0500

    extracted big getDiscount method with all offer types

--- src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -45,30 +45,16 @@ public class ShoppingCart {
         for (Product product: productQuantities().keySet()) {
             if (offers.containsKey(product)) {
                 Offer offer = offers.get(product);
-                Discount discount = null;
 
                 if (offer.offerType == SpecialOfferType.BundledDiscount) {
-                    discount = offer.getDiscount(catalog, product, this);
+                    Discount discount = offer.getDiscount(catalog, product, this);
+
+                    if (discount != null)
+                        receipt.addDiscount(discount);
                 }
 
-                if (offer.offerType == SpecialOfferType.TwoForAmount) {
-                    discount = getDiscount(catalog, product, offer);
+                Discount discount = getDiscount(catalog, product, offer);
 
-                }
-
-                if (offer.offerType == SpecialOfferType.ThreeForTwo && getQuantityAsInt(product) > 2) {
-                    int numberOfXs = getQuantityAsInt(product) / 3;
-                    double discountAmount = getQuantity(product) * catalog.getUnitPrice(product) - ((numberOfXs * 2 * catalog.getUnitPrice(product)) + getQuantityAsInt(product) % 3 * catalog.getUnitPrice(product));
-                    discount = new Discount(product, "3 for 2", -discountAmount);
-                }
-                if (offer.offerType == SpecialOfferType.PercentDiscount) {
-                    discount = new Discount(product, offer.discountPercentageOrAmount + "% off", -getQuantity(product) * catalog.getUnitPrice(product) * offer.discountPercentageOrAmount / 100.0);
-                }
-                if (offer.offerType == SpecialOfferType.FiveForAmount && getQuantityAsInt(product) >= 5) {
-                    int numberOfXs = getQuantityAsInt(product) / 5;
-                    double discountTotal = catalog.getUnitPrice(product) * getQuantity(product) - (offer.discountPercentageOrAmount * numberOfXs + getQuantityAsInt(product) % 5 * catalog.getUnitPrice(product));
-                    discount = new Discount(product, 5 + " for " + offer.discountPercentageOrAmount, -discountTotal);
-                }
                 if (discount != null)
                     receipt.addDiscount(discount);
             }
@@ -77,10 +63,28 @@ public class ShoppingCart {
     }
 
     private Discount getDiscount(SupermarketCatalog catalog, Product product, Offer offer) {
-        if (getQuantityAsInt(product) >= 2) {
-            double total = offer.discountPercentageOrAmount * (getQuantityAsInt(product) / 2) + getQuantityAsInt(product) % 2 * catalog.getUnitPrice(product);
-            double discountN = catalog.getUnitPrice(product) * getQuantity(product) - total;
-            return new Discount(product, "2 for " + offer.discountPercentageOrAmount, -discountN);
+        if (offer.offerType == SpecialOfferType.TwoForAmount) {
+            if (getQuantityAsInt(product) >= 2) {
+                double total = offer.discountPercentageOrAmount * (getQuantityAsInt(product) / 2) + getQuantityAsInt(
+                        product) % 2 * catalog.getUnitPrice(product);
+                double discountN = catalog.getUnitPrice(product) * getQuantity(product) - total;
+                return new Discount(product, "2 for " + offer.discountPercentageOrAmount, -discountN);
+            }
+            return null;
+        }
+
+        if (offer.offerType == SpecialOfferType.ThreeForTwo && getQuantityAsInt(product) > 2) {
+            int numberOfXs = getQuantityAsInt(product) / 3;
+            double discountAmount = getQuantity(product) * catalog.getUnitPrice(product) - ((numberOfXs * 2 * catalog.getUnitPrice(product)) + getQuantityAsInt(product) % 3 * catalog.getUnitPrice(product));
+            return new Discount(product, "3 for 2", -discountAmount);
+        }
+        if (offer.offerType == SpecialOfferType.PercentDiscount) {
+            return new Discount(product, offer.discountPercentageOrAmount + "% off", -getQuantity(product) * catalog.getUnitPrice(product) * offer.discountPercentageOrAmount / 100.0);
+        }
+        if (offer.offerType == SpecialOfferType.FiveForAmount && getQuantityAsInt(product) >= 5) {
+            int numberOfXs = getQuantityAsInt(product) / 5;
+            double discountTotal = catalog.getUnitPrice(product) * getQuantity(product) - (offer.discountPercentageOrAmount * numberOfXs + getQuantityAsInt(product) % 5 * catalog.getUnitPrice(product));
+            return new Discount(product, 5 + " for " + offer.discountPercentageOrAmount, -discountTotal);
         }
         return null;
     }
