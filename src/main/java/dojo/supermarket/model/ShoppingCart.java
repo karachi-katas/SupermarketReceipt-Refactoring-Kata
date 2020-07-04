0a7ec3f54a2baa7541b3ceac8f2d10aac78c647a
lamak <lamak.qaizar@careem.com>
2020-07-04 15:53:28 +0500

    removed common dependency on x

--- src/main/java/dojo/supermarket/model/ShoppingCart.java
+++ src/main/java/dojo/supermarket/model/ShoppingCart.java
@@ -54,27 +54,22 @@ public class ShoppingCart {
             discount = offer.getDiscount(product, quantity, unitPrice);
         }
         else {
-            int x = 1;
-            if (offer.offerType == SpecialOfferType.ThreeForTwo) {
-                x = 3;
-
-            } else if (offer.offerType == SpecialOfferType.TwoForAmount) {
-                x = 2;
-
-            } if (offer.offerType == SpecialOfferType.FiveForAmount) {
-                x = 5;
-            }
-            int numberOfXs = quantityAsInt / x;
             if (offer.offerType == SpecialOfferType.ThreeForTwo && quantityAsInt > 2) {
+                int x = 3;
+                int numberOfXs = quantityAsInt / x;
                 double discountAmount = quantity * unitPrice - ((numberOfXs * 2 * unitPrice) + quantityAsInt % 3 * unitPrice);
                 discount = new Discount(product, "3 for 2", -discountAmount);
             }
             if (offer.offerType == SpecialOfferType.TwoForAmount && quantityAsInt >= 2) {
+                int x = 2;
+                int numberOfXs = quantityAsInt / x;
                 double total = offer.argument * numberOfXs + quantityAsInt % 2 * unitPrice;
                 double discountN = unitPrice * quantity - total;
                 discount = new Discount(product, "2 for " + offer.argument, -discountN);
             }
             if (offer.offerType == SpecialOfferType.FiveForAmount && quantityAsInt >= 5) {
+                int x = 5;
+                int numberOfXs = quantityAsInt / x;
                 double discountTotal = unitPrice * quantity - (offer.argument * numberOfXs + quantityAsInt % 5 * unitPrice);
                 discount = new Discount(product, x + " for " + offer.argument, -discountTotal);
             }
